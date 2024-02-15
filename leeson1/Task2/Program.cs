354d2671117de08868e23ef80c641481d081f2ee
Nikolay Litvinchuk <nikolajlitvinchuk1985@gmail.com>
2024-02-15 16:56:35 +0700

    add lesson1:Task2

--- leeson1/Task2/Program.cs
+++ leeson1/Task2/Program.cs
@@ -17,3 +17,10 @@ double product3 = d1 * i1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int guotient = i1 / i2;
+Console.WriteLine(guotient);
+
+d1 = 6;
+double guotient2 = i1 / d1;
+Console.WriteLine(guotient2);
